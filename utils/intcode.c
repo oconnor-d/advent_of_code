3dd16ac312388ba2c410d4a6a3d7bfcc85e3421c
oconnor-d <oconnor.d@northeastern.edu>
2024-12-29 13:44:14 -0500

    add parameter modes to intcode

--- utils/intcode.c
+++ utils/intcode.c
@@ -7,6 +7,35 @@ An Intcode Computer.
 #include "array.c"
 #include "string.c"
 
+// ================================ Constants ================================
+
+// The max number of parameters any opcode has.
+#define MAX_OPCODE_PARAMETERS 3
+
+/*
+The mode an opcode's parameters should be evaluated in.
+
+Parameter Modes for each parameter for an opcode is stored in the same value as the opcode.
+The right-most two digits in the instruction are the opcode, and going left each digit is
+the mode of the parameter. Going left after the opcode, each digit is the mode of the next
+parameter. If there's no digit for a parameter, the mode defaults to 0.
+
+Example:
+
+ABCDE
+ 1002
+
+DE - two-digit opcode,      02 == opcode 2
+ C - mode of 1st parameter,  0 == position mode
+ B - mode of 2nd parameter,  1 == immediate mode
+ A - mode of 3rd parameter,  0 == position mode,
+                                  omitted due to being a leading zero
+*/
+typedef enum ParameterMode {
+    POSITION = 0,
+    IMMEDIATE = 1,
+} ParameterMode;
+
 typedef struct {
     int* program;
     int* memory;
@@ -24,6 +53,36 @@ typedef struct {
 // Immediately exit the Intcode program.
 #define EXIT 99
 
+// clang-format off
+// The lengths of each instruction's parameter list, not including the opcode itself.
+int INSTRUCTION_PARAMETER_LENGTHS[100] = {
+    0, 3, 3, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+// The index of an opcode's output parameter, if it has one.
+int INSTRUCTION_OUTPUT_PARAMETER_INDEXES[100] = {
+    -1, 2, 2, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+};
+// clang-format on
+
 void printIntCodeProgram(IntCodeProgram* program) {
     /*
     Nicely displays an intcode program and it's memory.
@@ -39,6 +98,7 @@ void printIntCodeProgram(IntCodeProgram* program) {
     printf("]\n");
     printf("Memory:  [");
     for (int idx = 0; idx < program->size; idx += 1) {
+        if (idx == program->instructionPointer) printf("->");
         if (idx == program->size - 1)
             printf("%d", program->memory[idx]);
         else
@@ -90,6 +150,72 @@ void freeIntCodeProgram(IntCodeProgram* program) {
     program->instructionPointer = 0;
 }
 
+int getOpcode(IntCodeProgram* program, ParameterMode* parameterModes) {
+    /*
+    Gets the opcode at the current instruction pointer of the program, storing the instruction's parameter
+    modes in the given array at the same time.
+
+        [1, 0, 0, 1, ->2102, ...] -> opcode=2, parameterModes=[1, 2, 0]
+
+    The instruction pointer MUST be pointing at an opcode.
+    */
+    int instruction = program->memory[program->instructionPointer];
+
+    // If the instruction is only one or two digits, the opcode is the instruction and the parameter
+    // modes are the implicit POSITION mode.
+    if (instruction < 100) {
+        for (int idx = 0; idx < MAX_OPCODE_PARAMETERS; idx += 1) parameterModes[idx] = POSITION;
+        return instruction;
+    }
+
+    int opcode = instruction % 100;
+
+    // Exclude the opcode from the instruction.
+    instruction /= 100;
+
+    // Load the parameter modes into the array.
+    int numDigits = MAX_OPCODE_PARAMETERS - 1;
+    while (numDigits >= 0) {
+        // Even though we're getting the digits from the instruction going forward, we want to store it
+        // going backward, so the mode for parameter 1 is at index 0, 2 is at index 1, etc.
+        parameterModes[MAX_OPCODE_PARAMETERS - 1 - numDigits] = (ParameterMode)instruction % 10;
+
+        instruction /= 10;
+        numDigits -= 1;
+    }
+
+    return opcode;
+}
+
+int resolveNextInstruction(IntCodeProgram* program, ParameterMode* parameterModes, int* parameters) {
+    /*
+    Resolves the instruction the pointer is currently on, getting the opcode and storing the mode-evaluated
+    parameters for the opcode in the parameter array.
+
+    Advances the instruction pointer past this instruction and it's parameters, to the next instruction
+    (or out of bounds if the instruction resolved was the last one in the program).
+
+    The instruction pointer MUST be pointing at an opcode to start.
+    */
+    int opcode = getOpcode(program, parameterModes);
+    program->instructionPointer += 1;
+
+    for (int idx = 0; idx < INSTRUCTION_PARAMETER_LENGTHS[opcode]; idx += 1) {
+        if (INSTRUCTION_OUTPUT_PARAMETER_INDEXES[opcode] == idx) {
+            // Return the actual output index, since the opcode handles storing it.
+            parameters[idx] = program->memory[program->instructionPointer];
+        } else if (parameterModes[idx] == POSITION) {
+            parameters[idx] = program->memory[program->memory[program->instructionPointer]];
+        } else if (parameterModes[idx] == IMMEDIATE) {
+            parameters[idx] = program->memory[program->instructionPointer];
+        }
+
+        program->instructionPointer += 1;
+    }
+
+    return opcode;
+}
+
 void intcodeRun(IntCodeProgram* program) {
     /*
     Runs the given intcode program, the end result is stored in the program's memory, while the original
@@ -101,30 +227,22 @@ void intcodeRun(IntCodeProgram* program) {
     // Copy the program into memory.
     for (int idx = 0; idx < program->size; idx += 1) program->memory[idx] = program->program[idx];
 
+    // The opcode being processed.
     int opcode;
-    // Potential opcode parameters.
-    int a, b, c;
+    // Potential opcode parameters and the modes they were evaluated in.
+    ParameterMode parameterModes[MAX_OPCODE_PARAMETERS];
+    int parameters[MAX_OPCODE_PARAMETERS];
     // Start from the beginning.
     program->instructionPointer = 0;
     while (program->instructionPointer >= 0 && program->instructionPointer < program->size) {
-        opcode = program->memory[program->instructionPointer];
+        // Resolve the next instruction - getting the opcode, storing the parameter values already evaluated
+        // as per their mode, and advancing the instruction pointer to the start of the next instruction.
+        opcode = resolveNextInstruction(program, parameterModes, parameters);
 
         if (opcode == ADD) {
-            a = program->memory[program->instructionPointer + 1];
-            b = program->memory[program->instructionPointer + 2];
-            c = program->memory[program->instructionPointer + 3];
-
-            program->memory[c] = program->memory[a] + program->memory[b];
-
-            program->instructionPointer += 4;
+            program->memory[parameters[2]] = parameters[0] + parameters[1];
         } else if (opcode == MULTIPLY) {
-            a = program->memory[program->instructionPointer + 1];
-            b = program->memory[program->instructionPointer + 2];
-            c = program->memory[program->instructionPointer + 3];
-
-            program->memory[c] = program->memory[a] * program->memory[b];
-
-            program->instructionPointer += 4;
+            program->memory[parameters[2]] = parameters[0] * parameters[1];
         } else if (opcode == EXIT) {
             // Immediately halt the program.
             return;
