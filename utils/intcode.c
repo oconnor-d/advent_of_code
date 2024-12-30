f51f701da5634cd76a67b8927ac9eb4efba7bd3b
oconnor-d <oconnor.d@northeastern.edu>
2024-12-29 22:34:25 -0500

    Update the intcode interpreter to stop to await more input, as opposed to halting when opcode 99 is hit.

--- utils/intcode.c
+++ utils/intcode.c
@@ -1,7 +1,13 @@
 /*
-An Intcode Computer.
+Interpreter for running Intcode Programs.
+
+An Intcode program is a list of integers representing opcodes and parameters for the opcodes to operate
+on. For example, 1,0,0,2,1,3,99 is an Intcode Program.
+
+Refer to the struct and `intcodeRun` docs for more details on how an Intcode program works.
 */
 
+#include <stdbool.h>
 #include <stdio.h>
 
 #include "array.c"
@@ -11,29 +17,6 @@ An Intcode Computer.
 
 // The max number of parameters any opcode has.
 #define MAX_OPCODE_PARAMETERS 3
-
-typedef enum OpCode {
-    // Adds the first two parameters, and store the result at the index
-    // given by the third number.
-    ADD = 1,
-    // Multiplies the first two parameters, and store the result at the index
-    // given by the third number.
-    MULTIPLY = 2,
-    // Stores the next input at the address of it's only parameter.
-    STORE_INPUT = 3,
-    // Outputs the value of it's only parameter.
-    OUTPUT = 4,
-    // Jumps to the address of the second parameter iff the first is non-zero.
-    JUMP_IF_TRUE = 5,
-    // Jumps to the address of the second parameter iff the first is zero.
-    JUMP_IF_FALSE = 6,
-    // Stores 1 in the third parameter address if the first is less than the second, 0 otherwise.
-    LESS_THAN = 7,
-    // Stores 1 in the third parameter address if the first is equal to the second, 0 otherwise.
-    EQUALS = 8,
-    // Immediately exit the program.
-    EXIT = 99,
-} OpCode;
 
 // clang-format off
 // The lengths of each instruction's parameter list, not including the opcode itself.
@@ -67,6 +50,29 @@ int INSTRUCTION_OUTPUT_PARAMETER_INDEXES[100] = {
 
 // ================================ Data ================================
 
+typedef enum OpCode {
+    // Adds the first two parameters, and store the result at the index
+    // given by the third number.
+    ADD = 1,
+    // Multiplies the first two parameters, and store the result at the index
+    // given by the third number.
+    MULTIPLY = 2,
+    // Stores the next input at the address of it's only parameter.
+    STORE_INPUT = 3,
+    // Outputs the value of it's only parameter.
+    OUTPUT = 4,
+    // Jumps to the address of the second parameter iff the first is non-zero.
+    JUMP_IF_TRUE = 5,
+    // Jumps to the address of the second parameter iff the first is zero.
+    JUMP_IF_FALSE = 6,
+    // Stores 1 in the third parameter address if the first is less than the second, 0 otherwise.
+    LESS_THAN = 7,
+    // Stores 1 in the third parameter address if the first is equal to the second, 0 otherwise.
+    EQUALS = 8,
+    // Immediately exit the program.
+    EXIT = 99,
+} OpCode;
+
 /*
 The mode an opcode's parameters should be evaluated in.
 
@@ -91,12 +97,33 @@ typedef enum ParameterMode {
     IMMEDIATE = 1,
 } ParameterMode;
 
+/*
+An Intcode Program, holding the source code, the program's working memory, where the program is at
+in evaluation, I/O, and more.
+
+Running:
+
+In general, the source code is held in `program`, and gets copied over into `memory` before the
+program is run. Once the program is run, `instructionPointer` points to the next instruction to be
+evaluated.
+
+If a program hits the EXIT opcode, it is marked as `halted`. There are times when the program stops
+running without fully halting, like when it's waiting on input.
+
+I/O:
+
+The `input` and `output` buffers handle the program's I/O. The input stores all current input to the
+program, and the `inputPointer` points to the next input to consume (like for a STORE_INPUT instruction).
+The output stores all outputted, with the most recent output being the last element in the buffer.
+*/
 typedef struct {
     int* program;
     int* memory;
 
     int size;
     int instructionPointer;
+    // If the program is halted, i.e. hit opcode 99 or has not yet been run, or not.
+    bool halted;
 
     int inputPointer;
     IntArray input;
@@ -109,7 +136,7 @@ void printIntCodeProgram(IntCodeProgram* program) {
     /*
     Nicely displays an intcode program and it's memory.
     */
-    printf("[Size=%d, Instruction Pointer=%d]\n", program->size, program->instructionPointer);
+    printf("[Size=%d, Instruction Pointer=%d, Halted=%d]\n", program->size, program->instructionPointer, program->halted);
     printf("Program: [");
     for (int idx = 0; idx < program->size; idx += 1) {
         if (idx == program->size - 1)
@@ -126,7 +153,14 @@ void printIntCodeProgram(IntCodeProgram* program) {
         else
             printf("%d, ", program->memory[idx]);
     }
-    printf("]\n\n");
+    printf("]\n");
+
+    printf("Input: ");
+    printIntArray(&program->input);
+
+    printf("Output: ");
+    printIntArray(&program->input);
+    printf("\n");
 }
 
 void initIntCodeProgramFromIntArray(IntCodeProgram* program, IntArray* array) {
@@ -139,6 +173,8 @@ void initIntCodeProgramFromIntArray(IntCodeProgram* program, IntArray* array) {
 
     program->size = array->numItems;
     program->instructionPointer = 0;
+    // A never-before-run program starts off as halted, until it's run for the first time.
+    program->halted = true;
 
     initIntArray(&program->input, 1024);
     initIntArray(&program->output, 1024);
@@ -168,6 +204,9 @@ void initIntCodeProgramFromFile(IntCodeProgram* program, char* inputFilePath) {
 }
 
 void freeIntCodeProgram(IntCodeProgram* program) {
+    /*
+    Frees all memory associated with the program.
+    */
     free(program->program);
     program->program = NULL;
     free(program->memory);
@@ -255,16 +294,24 @@ void intcodeRun(IntCodeProgram* program) {
     Before running, copies the program into memory, overriding any previous memory the program may have.
     */
 
-    // Copy the program into memory.
-    for (int idx = 0; idx < program->size; idx += 1) program->memory[idx] = program->program[idx];
+    // If the program to run has halted, reset it to run from the beginning, otherwise, the program
+    // will be run from the instruction pointer it left off at.
+    if (program->halted) {
+        // Copy the program into memory.
+        for (int idx = 0; idx < program->size; idx += 1) program->memory[idx] = program->program[idx];
+
+        // Reset pointers and output.
+        program->inputPointer = 0;
+        program->output.numItems = 0;
+        program->instructionPointer = 0;
+        program->halted = false;
+    }
 
     // The opcode being processed.
     int opcode;
     // Potential opcode parameters and the modes they were evaluated in.
     ParameterMode parameterModes[MAX_OPCODE_PARAMETERS];
     int parameters[MAX_OPCODE_PARAMETERS];
-    // Start from the beginning.
-    program->instructionPointer = 0;
     while (program->instructionPointer >= 0 && program->instructionPointer < program->size) {
         // Resolve the next instruction - getting the opcode, storing the parameter values already evaluated
         // as per their mode, and advancing the instruction pointer to the start of the next instruction.
@@ -275,6 +322,13 @@ void intcodeRun(IntCodeProgram* program) {
         } else if (opcode == MULTIPLY) {
             program->memory[parameters[2]] = parameters[0] * parameters[1];
         } else if (opcode == STORE_INPUT) {
+            if (program->inputPointer >= program->input.numItems) {
+                // Waiting on input. Reset the pointer to the start of this instruction and return early. The
+                // next time the program is run it will resume from this instruction, potentially with new input.
+                program->instructionPointer -= 2;
+                return;
+            }
+
             program->memory[parameters[0]] = program->input.data[program->inputPointer];
             program->inputPointer += 1;
         } else if (opcode == OUTPUT) {
@@ -288,7 +342,8 @@ void intcodeRun(IntCodeProgram* program) {
         } else if (opcode == EQUALS) {
             program->memory[parameters[2]] = parameters[0] == parameters[1] ? 1 : 0;
         } else if (opcode == EXIT) {
-            // Immediately halt the program.
+            // Immediately halt the program, and mark it as halted.
+            program->halted = true;
             return;
         }
     }
